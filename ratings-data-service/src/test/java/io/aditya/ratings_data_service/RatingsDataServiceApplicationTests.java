3d36cea59f01b8c5513ebb5d5f14b7772ff78eaa
Aditya Keshari <adityakeshari2002@gmail.com>
2024-11-16 15:03:09 +0530

    Add files via upload

---
+++ ratings-data-service/src/test/java/io/aditya/ratings_data_service/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aditya.ratings_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
